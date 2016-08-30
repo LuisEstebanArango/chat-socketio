3a9b31b0a503f618bc74488c73d1d94ff0eeab8f
luisesteban <estebanarango14@hotmail.com>
2016-08-30 05:23:31 -0500

    Se reestructura el proyecto

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,16 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+// path para utilizar /../ en la ruta del sendFile
 var path = require('path');
 
+// para servir los archivos estaticos
+app.use('/bower', express.static(path.resolve(__dirname + '/../frontend/bower_components')));
+
 app.get('/', function(req, res){
-  res.sendFile(path.resolve(__dirname + '/../frontend/' + '/index.html'));
+  res.sendFile(path.resolve(__dirname + '/../frontend/app/index.html'));
 });
 
 io.on('connection', function(socket){
